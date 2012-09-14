9042ee298d73f729a49f08c92911b31e2f786714
Mikko Rönkkö <mikko.ronkko@iki.fi>
2012-09-14 08:30:49 +0300

    Initial Commit

---
+++ CSLParser/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CSLParser
+//
+//  Created by Rönkkö Mikko on 9/14/12.
+//  Copyright (c) 2012 Rönkkö Mikko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
