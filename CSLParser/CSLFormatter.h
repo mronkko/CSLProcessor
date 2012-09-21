c2641e20b3659da094b632e6e516064679d23976
Mikko Rönkkö <mikko.ronkko@iki.fi>
2012-09-21 10:00:19 +0300

    First commit

---
+++ CSLParser/CSLFormatter.h
@@ -0,0 +1,13 @@
+//
+//  CSLFormatter.h
+//  CSLParser
+//
+//  Created by Rönkkö Mikko on 9/14/12.
+//  Copyright (c) 2012 Rönkkö Mikko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSLFormatter : NSObject
+
+@end
---
+++ CSLParser/CSLFormatter.m
@@ -0,0 +1,13 @@
+//
+//  CSLFormatter.m
+//  CSLParser
+//
+//  Created by Rönkkö Mikko on 9/14/12.
+//  Copyright (c) 2012 Rönkkö Mikko. All rights reserved.
+//
+
+#import "CSLFormatter.h"
+
+@implementation CSLFormatter
+
+@end
