603337374b30f781d8b7401c6a0553a5b0064d1b
Mikko Rönkkö <mikko.ronkko@iki.fi>
2012-11-06 17:59:43 +0200

    Added missing files

---
+++ CSLProcessor/CSLFormatter.h
@@ -0,0 +1,40 @@
+//
+//  CSLFormatter.h
+//  CSLParser
+//
+//  Created by Rönkkö Mikko on 9/14/12.
+//  Copyright (c) 2012 Rönkkö Mikko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CSLFormatter : NSObject{
+    NSDictionary* fieldMap;
+    NSDictionary* typeMap;
+}
+
+/*
+
+ Initializes the formatter
+ 
+ @param cslPath path to a csl file
+ @param localePath path to a locale file
+ @param fieldMap path to an XML file containing field mapping.
+ 
+ */
+
+-(id) initWithCSLFile:(NSString*)cslPath localeFile:(NSString*)localePath fieldMapFile:(NSString*)fieldMapPath;
+
+/*
+
+ Returns a formatted bibliography item.
+ 
+ @param creators an array of dictionaries created from Zotero server response
+ @param fields a dictionary of fields created from Zotero server response
+ 
+ */
+
+-(NSString*) formatBibliographyItemUsingVariables:(NSDictionary*)variables;
+-(NSString*) formatBibliographyItemUsingVariables:(NSDictionary*)variables storeMacrosInDictionary:(NSMutableDictionary*) macros;
+
+@end
---
+++ CSLProcessor/main.m
@@ -0,0 +1,79 @@
+//
+//  main.m
+//  CSLParser
+//
+//  Created by Rönkkö Mikko on 9/14/12.
+//  Copyright (c) 2012 Rönkkö Mikko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "../json-framework/Classes/SBJson.h"
+#import "CSLFormatter.h"
+#import "NSString+HTML.h"
+
+int main(int argc, const char * argv[])
+{
+    
+    NSLog(@"Started");
+    NSString *path = [[NSBundle mainBundle] pathForResource: @"json" ofType: @"txt"];
+    NSString *fileContents = [NSString stringWithContentsOfFile:path];
+    NSArray *lines = [fileContents componentsSeparatedByString:@"\n"];
+
+    
+    NSString *citationsPath = [[NSBundle mainBundle] pathForResource: @"citations" ofType: @"txt"];
+    NSString *citationsFileContents = [NSString stringWithContentsOfFile:citationsPath];
+    NSArray *citationsLines = [citationsFileContents componentsSeparatedByString:@"\n"];
+
+    CSLFormatter* formatter=[[CSLFormatter alloc] initWithCSLFile:[[NSBundle mainBundle] pathForResource: @"apa" ofType: @"csl"]
+                                                           localeFile:[[NSBundle mainBundle] pathForResource: @"locales-en-US" ofType: @"xml"]
+                                                         fieldMapFile:[[NSBundle mainBundle] pathForResource: @"typeMap" ofType: @"xml"]];
+
+    NSInteger i = 0;
+    for(NSString* line in lines){
+        
+
+        NSMutableDictionary* fields = [NSMutableDictionary dictionaryWithDictionary:[line JSONValue]];
+        
+        //Get creators as separate variables
+        for(NSDictionary* creator in [fields objectForKey:@"creators"]){
+            NSString* type = [creator objectForKey:@"creatorType"];
+            
+            NSMutableArray* creatorArray = [fields objectForKey:type];
+            if(creatorArray == NULL){
+                creatorArray = [[NSMutableArray alloc] init];
+                [fields setObject:creatorArray forKey:type];
+            }
+            [creatorArray addObject:creator];
+        }
+        
+        NSString* citation = [citationsLines objectAtIndex:i++];
+        citation = [citation stringByDecodingHTMLEntities];
+        
+        NSMutableDictionary* macroDict = [[NSMutableDictionary alloc] init];
+        NSString* generatedCitation = [formatter formatBibliographyItemUsingVariables:fields storeMacrosInDictionary:macroDict];
+        
+        generatedCitation = [generatedCitation stringByConvertingHTMLToPlainText];
+        citation = [citation stringByConvertingHTMLToPlainText];
+        
+        if(! [generatedCitation isEqualToString:citation]){
+            NSLog(@"\n%i\n%@\n\nGenerated: '%@'\nOriginal:  '%@'\n\n",i, line, generatedCitation,citation);
+            
+            NSInteger limit = MIN([generatedCitation length],[citation length]);
+            
+            for(NSInteger j=0;j<limit;++j){
+                unichar g = [generatedCitation characterAtIndex:j];
+                unichar o = [citation characterAtIndex:j];
+                
+                if(o!=g){
+                    NSString* charString = [generatedCitation substringWithRange:NSMakeRange(j, 1)];
+                    NSLog(@"Character at %i (%@) differs. Original: %i Generated: %i",j,charString,(NSInteger) o, (NSInteger) g);
+                }
+            }
+                                  
+            break;
+        }
+    }
+    NSLog(@"Done");
+        
+    return 0;
+}
